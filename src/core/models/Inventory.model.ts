8d6d5e28fa8b13e53c81e2df2e1c972b9f4ac6cc
William Silva <williamsilva808@hotmail.com>
2022-03-03 14:15:16 -0300

    Add some comment on Inventory class.

--- src/core/models/Inventory.model.ts
+++ src/core/models/Inventory.model.ts
@@ -3,13 +3,17 @@ import IEelement from "./element.model";
 export default class Inventory<T extends IEelement> {
   private readonly elements: Array<T> = [];
 
+  /**
+   * to add a new element
+   * @param element T
+   */
   add(element: T): void {
     this.elements.push(element);
   }
 
   /**
    * Check if contains element into array
-   * @param element
+   * @param element T
    * @returns boolean
    */
   contains(element: T): boolean {
@@ -18,6 +22,11 @@ export default class Inventory<T extends IEelement> {
     );
   }
 
+  /**
+   * get position from a specific element.
+   * @param element T
+   * @returns number
+   */
   position(element: T): number {
     const elementOnInventory = this.elements.find(
       (el) => el.equals(element) === true
@@ -30,6 +39,10 @@ export default class Inventory<T extends IEelement> {
     return this.elements.indexOf(elementOnInventory);
   }
 
+  /**
+   * return a list of items
+   * @returns Array<T>
+   */
   toArray(): Array<T> {
     return this.elements;
   }
